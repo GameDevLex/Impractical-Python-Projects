968c919c1342d002fe72ae9d6878b5bebeed627c
GameDevLex <34077143+GameDevLex@users.noreply.github.com>
2025-08-05 18:26:19 -0500

    Added scrape palingram function in chapter 2

--- chapter_2.py
+++ chapter_2.py
@@ -19,5 +19,24 @@ def scrape_palidrome():
         print("Error: The file 'your_file.txt' was not found.")
 
 
+def scrape_palingram():
+    try:
+        lines = []
+        palingram = []
+        with open('dictionary.txt') as file:
+            while line := file.readline():
+                lines.append(line.rstrip())
+
+        for line in lines:
+            if (" " in line) and (line.replace(" ", "") == (line.replace(" ", ""))[::-1]):
+                palingram.append(line)
+
+        print(palingram)
+    except FileNotFoundError:
+        print("Error: The file 'your_file.txt' was not found.")
+
+
+
 if __name__ == '__main__':
     scrape_palidrome()
+    scrape_palingram()
