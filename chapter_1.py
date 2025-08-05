bca4ae0147b81507caaec1da807c54bfcb60459b
GameDevLex <34077143+GameDevLex@users.noreply.github.com>
2025-08-05 01:02:12 -0500

    Added pig latin function for chapter 1

--- chapter_1.py
+++ chapter_1.py
@@ -1,6 +1,7 @@
 import sys, random
 
-if __name__ == '__main__':
+
+def funny_name_generator():
     first_names = ("John", "Joey", "David", "Paul", "Nathan", "Ben")
     last_names = ("Rajan", "Tsu", "Kopf", "Paul", "Beer", "O'Connel")
 
@@ -15,3 +16,23 @@ if __name__ == '__main__':
         try_again = input("\n" + "Generate another random name? (Type exit to close app)" + "\n")
         if try_again == "exit":
             break
+
+def pig_latin():
+    while True:
+        input_word = input("Word to convert to pig latin: ")
+        pig_latin = ""
+        for i in reversed(input_word):
+            pig_latin = pig_latin + i
+        pig_latin = pig_latin + "ay"
+        print(pig_latin)
+
+        input_word = input("Continue? (Write cancel to close)")
+        if input_word.lower() == "cancel":
+            break
+        else:
+            pass
+
+
+if __name__ == '__main__':
+    funny_name_generator()
+    pig_latin()
