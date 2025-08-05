2b5603de634cb4ac524de1fc8dedd2f284d5b087
GameDevLex <34077143+GameDevLex@users.noreply.github.com>
2025-08-05 00:12:35 -0500

    Finished chapter 1 random name generator script

--- chapter_1.py
+++ chapter_1.py
@@ -1,2 +1,17 @@
-def test():
-    print("TESTING")
+import sys, random
+
+if __name__ == '__main__':
+    first_names = ("John", "Joey", "David", "Paul", "Nathan", "Ben")
+    last_names = ("Rajan", "Tsu", "Kopf", "Paul", "Beer", "O'Connel")
+
+    while True:
+        first_name = random.choice(first_names)
+        last_name = random.choice(last_names)
+
+        print("\n")
+        print(first_name + " " + last_name, file=sys.stderr)
+        print("\n")
+
+        try_again = input("\n" + "Generate another random name? (Type exit to close app)" + "\n")
+        if try_again == "exit":
+            break
--- main.py
+++ main.py
@@ -3,15 +3,14 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import chapter_1;
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import chapter_2;
+import chapter_3;
+import chapter_4;
+import chapter_5;
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    chapter_1.test()
+    pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
